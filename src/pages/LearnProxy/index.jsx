4814d8b6678830213314ea2374c52f7b4aa92406
enson_chen <1004714132@qq.com>
2023-10-18 16:22:53 +0800

    feat: ✨ 添加 proxy 案例

--- src/pages/LearnProxy/index.jsx
+++ src/pages/LearnProxy/index.jsx
@@ -6,12 +6,35 @@ function LearnProxy() {
    * Proxy 对象用于创建一个对象的代理，从而实现基本操作的拦截和自定义（如属性查找、赋值、枚举、函数调用等）。
    */
 
-  const a = {};
+  const a = {
+    // bar: { a: 1 }
+  };
+
+  // const reactive = (originObj) => {
+  //   if (typeof originObj !== 'object' || originObj === null) {
+  //     return originObj
+  //   }
+
+  //   return new Proxy(originObj, {
+  //     get: (obj, prop) => {
+  //       console.log(`监听到 get 方法`)
+  //       return reactive(obj[prop]);
+  //     },
+  //     set: (obj, prop, value) => {
+  //       console.log(`监听到 set 方法`)
+  //       // The default behavior to store the value
+  //       obj[prop] = value;
+  
+  //       // 表示成功
+  //       return true;
+  //     }
+  //   })
+  // }
 
   const p = new Proxy(a, {
     get: (obj, prop) => {
       console.log(`监听到 get 方法`)
-      return obj[prop];
+      return (obj[prop]);
     },
     set: (obj, prop, value) => {
       console.log(`监听到 set 方法`)
@@ -24,9 +47,11 @@ function LearnProxy() {
   })
 
   const handleClick = () => {
-    p.name = 1;
-
-    console.log(p.name);
+    p.name = 2;
+    console.log(p.name) 
+    
+    // p.bar.a = 2;
+    // console.log(p.bar.a);
   }
   return (
     <div>
